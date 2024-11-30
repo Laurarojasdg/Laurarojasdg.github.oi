071bf0f9b38aafdbc58ce2072e48f2313f3f745c
Laurarojasdg <77164364+Laurarojasdg@users.noreply.github.com>
2024-11-29 21:31:35 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+function nombrePreguntaPsicología() {
+
+    var nombrePreguntaPsicología = prompt("Juegos de rol:Ayudan a los niños a comprender las emociones y situaciones ajenas.")
+
+if (nombrePreguntaPsicología == "verdadero") {
+    alert("La respuesta es correcta");
+} else {
+    alert("La respuesta es incorrecta");
+}
+    
+}
+
+
+function nombrePreguntaNeuropsicología() {
+
+    var nombrePreguntaNeuropsicología = prompt("Dinámicas de compartir:Estimulan la interacción social y la actividad física.")
+
+if (nombrePreguntaNeuropsicología == "falso") {
+    alert("La respuesta es correcta");
+} else {
+    alert("La respuesta es incorrecta");
+}
+    
+}
+
+function nombrePreguntaSensopercepción() {
+
+    var nombrePreguntaSensopercepción = prompt("Teatro en grupo:Ayuda a los niños a mejorar la expresión y el trabajo en grupo")
+
+if (nombrePreguntaSensopercepción == "verdadero") {
+    alert("La respuesta es correcta");
+} else {
+    alert("La respuesta es incorrecta");
+}
+    
+}
